1d303521b3810071408bf2d0c2dec8f31c2bc57e
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-23 16:02:14 +0100

    Add responsive navigation bar with hamburger icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,41 @@
 import React from "react";
-//  q
+import { FaBars } from "react-icons/fa";
 import rp from "../assets/rp.png";
-
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-grey-300 ">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#262626] text-gray-300 ">
       <div>
-        <img src={rp} alt='Logo Image' style={{width: '100px'}} />
+        <img src={rp} alt="Logo Image" style={{ width: "100px" }} />
       </div>
+
+      {/* main-menu */}
+      <div>
+        <ul className="flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+        </ul>
+      </div>
+
+      {/* Hamburger */}
+      <div className="hidden">
+        <FaBars />
+      </div>
+
+      {/* mobile-menu */}
+      <ul className="hidden">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* Social icons */}
+      <div></div>
     </div>
   );
 };
