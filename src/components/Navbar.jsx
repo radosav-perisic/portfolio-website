0d103d4043b7265a5464ca6f3a30d73296f32fc4
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-23 14:37:19 +0100

    Add custom logo to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+//  q
+import rp from "../assets/rp.png";
+
 
 const Navbar = () => {
   return (
-    <div  className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'></div>
-  )
-}
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-grey-300 ">
+      <div>
+        <img src={rp} alt='Logo Image' style={{width: '100px'}} />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
-
-  
+export default Navbar;
