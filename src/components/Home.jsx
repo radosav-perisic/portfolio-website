e8ad62874f5699b956181e22696bbd0c8125969f
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-24 13:47:58 +0100

    Updated Home Page with Button for Work Showcase

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,12 @@ function Home() {
       {/* Cointainer */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-[#ca8a04]">Hi, my name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Radosav Perisic</h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a React Front End Engineer.</h2>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Radosav Perisic
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a React Front End Engineer.
+        </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           As a front-end engineer, I have abundant experience working with
           JavaScript, React.js, TypeScript, and Next.js. I specialize in using
@@ -19,6 +23,14 @@ function Home() {
           to detail and ability to communicate effectively with designers,
           developers, and other stakeholders make me an excellent team player.
         </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#ca8a04] hover:border-[#ca8a04]">
+            View Work{" "}
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   );
