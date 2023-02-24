ba11516ab92c424f313dcc8189c23934eb070788
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-24 11:22:21 +0100

    Added Email social icon to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,6 +51,11 @@ const Navbar = () => {
            Github <FaGithub size={30}/>
           </a>
         </li>
+        <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+          <a className="flex justify-between items-center w-full text-gray-300" href="/">
+           Email <HiOutlineMail size={30}/>
+          </a>
+        </li>
        </ul>
       </div>
     </div>
