3857a54626c6afb42f93c5981cad35ce12ba338d
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-24 10:23:15 +0100

    Add conditional rendering to mobile menu in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import { FaBars } from "react-icons/fa";
 import rp from "../assets/rp.png";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const clickHandler = () => setNav(!nav)
+
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#262626] text-gray-300 ">
       <div>
@@ -10,28 +13,27 @@ const Navbar = () => {
       </div>
 
       {/* main-menu */}
-      <div className="hidden md:flex">
-        <ul className="hidden md:flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
-      </div>
 
-      {/* Hamburger */}
-      <div className="hidden">
-        <FaBars />
-      </div>
-
-      {/* mobile-menu */}
-      <ul className="hidden">
+      <ul className="hidden md:flex">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
         <li>Work</li>
         <li>Contact</li>
+      </ul>
+
+      {/* Hamburger */}
+      <div onClick={clickHandler} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* mobile-menu */}
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
       </ul>
 
       {/* Social icons */}
