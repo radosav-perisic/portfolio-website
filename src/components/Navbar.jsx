93b63fcb54c391631e2d3062841a4f5226e0727d
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-24 11:17:44 +0100

    Added Linkedin social icon to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 import rp from "../assets/rp.png";
 
 const Navbar = () => {
@@ -37,7 +39,15 @@ const Navbar = () => {
       </ul>
 
       {/* Social icons */}
-      <div className="hidden"></div>
+      <div className="flex fixed flex-col top-[35%] left-0">
+       <ul>
+        <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+          <a className="flex justify-between items-center w-full text-gray-300" href="/">
+           Linkedin <FaLinkedin size={30}/>
+          </a>
+        </li>
+       </ul>
+      </div>
     </div>
   );
 };
