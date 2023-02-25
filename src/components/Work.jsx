16bd308e2563bcec4205906fce3f803992cc2029
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-25 18:39:27 +0100

    Add initial layout for Work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Work = () => {
   return (
-    <div className='bg-[#0a192f] w-full md-h-screen text-[#bcbcb3]'>
+    <div className='bg-[#262626] w-full md-h-screen text-[#bcbcb3]'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8 '>
-                <p className='text-4xl'>Work</p>
-                <p>// Check out some of my recent work</p>
+                <p className='text-4xl font-bold inline border-b-4 text-[#bcbcb3] border-color-[#ca8a04]'>Work</p>
+                <p className='py-6'>// Check out some of my recent work</p>
             </div>
-            <div>
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
                 <div>
                     <div>
                         <div>
