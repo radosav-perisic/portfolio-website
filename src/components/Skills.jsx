6d3a1a9afb8dcb322038b9eab6380c18afc0bcc5
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-27 10:02:05 +0100

    Updated skill icons and increased their size.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -21,29 +21,29 @@ const Skills = () => {
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8">
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Javascript} alt="HTML icon" />
-            <p>Javascript</p>
+          <div className="shadow-md py-10 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-24 mx-auto" src={Javascript} alt="HTML icon" />
+            <p className='pt-4 text-xl'>Javascript</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
-            <p>React</p>
+          <div className="shadow-md py-10 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-24 mx-auto" src={ReactImg} alt="HTML icon" />
+            <p className='pt-4 text-xl'>React</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Typescript} alt="HTML icon" />
-            <p>Typescript</p>
+          <div className="shadow-md py-10 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-24 mx-auto" src={Typescript} alt="HTML icon" />
+            <p className='pt-4 text-xl'>Typescript</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
-            <p>Tailwind</p>
+          <div className="shadow-md py-10 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-24 mx-auto" src={Tailwind} alt="HTML icon" />
+            <p className='pt-4 text-xl'>Tailwind</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
-            <p>HTML</p>
+          <div className="shadow-md py-10 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-24 mx-auto" src={HTML} alt="HTML icon" />
+            <p className='pt-4 text-xl'>HTML</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={CSS} alt="HTML icon" />
-            <p>CSS</p>
+          <div className="shadow-md py-10 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-24 mx-auto" src={CSS} alt="HTML icon" />
+            <p className='pt-4 text-xl'>CSS</p>
           </div>
         </div>
       </div>
