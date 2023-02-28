80bd8dffc51a21a6d1f8af24fd3b5936a22ec6a0
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-28 08:34:23 +0100

    Changed Contact component form action to use getform.io

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const  Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#262626] flex justify-center items-center p-4'>
-    <form action='' className='flex flex-col max-w-[600px] w-full'>
+    <form method='POST' action='https://getform.io/f/458cfa83-c534-4402-b446-978e8a78cd76' className='flex flex-col max-w-[600px] w-full'>
          <div className='pb-8'>
             <p className='text-4xl font-bold inline  border-b-4 border-[#ca8a04] text-[#bcbcb3]'>Contact</p>
             <p className='text-[#bcbcb3] py-4 font-sans'>// Submit the form below or send me an email - radosavperisic00@gmail.com</p>
