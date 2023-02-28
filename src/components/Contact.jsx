66e64eac75f8a1be3786e655d951c7338d090271
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-28 07:55:39 +0100

    Updated Contact component with name input

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,9 +5,10 @@ const  Contact = () => {
     <div name='contact' className='w-full h-screen bg-[#262626] flex justify-center items-center p-4'>
     <form action='' className='flex flex-col max-w-[600px] w-full'>
          <div className='pb-8'>
-            <p className='text-4xl font-bold inline border-b-4 border-[#ca8a04] text-[#bcbcb3]'>Contact</p>
+            <p className='text-4xl font-bold inline  border-b-4 border-[#ca8a04] text-[#bcbcb3]'>Contact</p>
             <p className='text-[#bcbcb3] py-4 font-sans'>// Submit the form below or send me an email - radosavperisic00@gmail.com</p>
          </div>
+         <input  type='text' name='name' placeholder='Name'className='bg-[#ccd6f6] p-2'/>
     </form>
 
     </div>
