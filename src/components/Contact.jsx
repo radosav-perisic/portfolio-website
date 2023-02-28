b0b20455bdd2187cd917fdbedcd6835cc6dcdd33
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-28 13:01:41 +0100

    Font change in Contact email for better visibility

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 const  Contact = () => {
+ const number = <span className='font-sans'>00</span>
+
   return (
     <div name='contact' className='w-full h-screen bg-[#262626] flex justify-center items-center p-4'>
     <form method='POST' action='https://getform.io/f/458cfa83-c534-4402-b446-978e8a78cd76' className='flex flex-col max-w-[600px] w-full'>
          <div className='pb-8'>
             <p className='text-4xl font-bold inline  border-b-4 border-[#ca8a04] text-[#bcbcb3]'>Contact</p>
-            <p className='text-[#bcbcb3] py-4 font-sans'>// Submit the form below or send me an email - radosavperisic00@gmail.com</p>
+            <p className='text-[#bcbcb3] py-4 overflow-hidden white-space: nowrap'>// Submit the form below or send me an email - radosavperisic{number}@gmail.com</p>
          </div>
          <input  type='text' name='name' placeholder='Name'className='bg-[#ccd6f6] p-2'/>
          <input type='email ' name='email' placeholder='Email' className='my-4 p-2 bg-[#ccd6f6]'/>
