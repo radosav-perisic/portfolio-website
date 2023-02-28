86cb1e4b2588ca4e4fb8c4c0601369da0bdac361
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-28 07:45:48 +0100

    Extend App with Contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const  Contact = () => {
+  return (
+    <div name='contact' className='w-full h-screen bg-[#262626] flex justify-center items-center p-4'>
+    <form action='' className='flex flex-col max-w-[600px] w-full'>
+         <div className='pb-8'>
+            <p className='text-4xl font-bold inline border border-b-4 border-[#ca8a04] text-[#bcbcb3]'>Contact</p>
+     </div>
+    </form>
+
+    </div>
+  ) 
+}
+
+export default Contact
