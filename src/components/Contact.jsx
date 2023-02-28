f1ac8479855c8450f82e12ac779ae7673a6b7eaf
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-28 07:52:57 +0100

    Added email contact details to contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,8 +5,9 @@ const  Contact = () => {
     <div name='contact' className='w-full h-screen bg-[#262626] flex justify-center items-center p-4'>
     <form action='' className='flex flex-col max-w-[600px] w-full'>
          <div className='pb-8'>
-            <p className='text-4xl font-bold inline border border-b-4 border-[#ca8a04] text-[#bcbcb3]'>Contact</p>
-     </div>
+            <p className='text-4xl font-bold inline border-b-4 border-[#ca8a04] text-[#bcbcb3]'>Contact</p>
+            <p className='text-[#bcbcb3] py-4 font-sans'>// Submit the form below or send me an email - radosavperisic00@gmail.com</p>
+         </div>
     </form>
 
     </div>
