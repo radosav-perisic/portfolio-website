947c8fd0733f16001c9e12b40e523cd0b1b90d55
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-01 15:43:51 +0100

    Adding Links to Navbar socials
    Links are added for my personal Github, Linkedin, and E-mail.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
           </Link>
         </li>
         <li>
-          <Link to="home" smooth={true} duration={500}>
+          <Link to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
@@ -97,7 +97,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://www.linkedin.com/in/radosav-perisic-58ab96265/"
             >
               Linkedin <FaLinkedin size={30} />
             </a>
@@ -105,7 +105,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://github.com/radosav-perisic"
             >
               Github <FaGithub size={30} />
             </a>
@@ -113,7 +113,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#f97316]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="mailto:radosavperisic00@gmail.com"
             >
               Email <HiOutlineMail size={30} />
             </a>
