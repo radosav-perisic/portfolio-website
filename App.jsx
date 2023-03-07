139ca9facf1f346b6856eac0a4b1d655a5fe5935
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-07 10:13:27 +0100

    Improved file managment

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import Home from "./src/components/Home"
+import Navbar from "./src/components/Navbar"
+import About from "./src/components/About"
+import Skills from "./src/components/Skills"
+import Work from "./src/components/Work"
+import Contact from "./src/components/Contact"
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
