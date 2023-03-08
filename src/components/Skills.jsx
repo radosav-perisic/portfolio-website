835a3cd0a62656d73e2fe1d9d3c5214fc17cb27f
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-08 13:26:25 +0100

    Improve Skills content spacing on mobile

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,7 +10,7 @@ const Skills = () => {
   return (
     <div name="skills" className="bg-[#262626] w-full h-screen text-[#bcbcb3]">
       {/*Container*/}
-      <div className="max-w-[1000px] mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] md:p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-[#ca8a04]">
             Skills
@@ -21,27 +21,27 @@ const Skills = () => {
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8">
-          <div className="shadow-md py-10 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-md lg:py-5 sm:py-2 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-24 mx-auto" src={Javascript} alt="HTML icon" />
             <p className='pt-4 text-xl'>Javascript</p>
           </div>
-          <div className="shadow-md py-10 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-md lg:py-5 sm:py-2 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-24 mx-auto" src={ReactImg} alt="HTML icon" />
             <p className='pt-4 text-xl'>React</p>
           </div>
-          <div className="shadow-md py-10 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-md lg:py-5 sm:py-2 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-24 mx-auto" src={Typescript} alt="HTML icon" />
             <p className='pt-4 text-xl'>Typescript</p>
           </div>
-          <div className="shadow-md py-10 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-md lg:py-5 sm:py-2 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-24 mx-auto" src={Tailwind} alt="HTML icon" />
             <p className='pt-4 text-xl'>Tailwind</p>
           </div>
-          <div className="shadow-md py-10 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-md lg:py-5 sm:py-2 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-24 mx-auto" src={HTML} alt="HTML icon" />
             <p className='pt-4 text-xl'>HTML</p>
           </div>
-          <div className="shadow-md py-10 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-md lg:py-5 sm:py-2 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-24 mx-auto" src={CSS} alt="HTML icon" />
             <p className='pt-4 text-xl'>CSS</p>
           </div>
