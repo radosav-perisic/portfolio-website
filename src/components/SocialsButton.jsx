e4f62024f88b7a3f2f25ca2cb1ebfeaa63734234
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-09 12:40:12 +0100

    Added SocialsButton component

---
+++ src/components/SocialsButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SocialsButton = () => {
+  return (
+    <button className='bg-[#ca8a04] z-10 text-white font-bold py-2 px-4 rounded-full'>Show Socials</button>
+  )
+}
+
+export default SocialsButton
