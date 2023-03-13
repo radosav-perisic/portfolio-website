c2abeaf224c7421cc6fab8b745ca86eca9156131
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-13 12:14:37 +0100

    Updated skills list on homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
         </h2>
         <p className="text-[#adaf92] py-4 max-w-[700px]">
           As a front-end engineer, I have abundant experience working with
-          JavaScript, React.js, TypeScript, and Next.js. I specialize in using
+          JavaScript, React.js, TypeScript, and Tailwind. I specialize in using
           React hooks to manage state and avoid the need for external libraries
           like Redux. With a deep understanding of both the theoretical and
           practical aspects of front-end development, I excel at creating
