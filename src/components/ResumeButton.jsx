eba989ae470c97e089bca80a4fb9a382c3b63895
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-14 22:48:44 +0100

    Update ResumeButton to wrap button in anchor tag

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const ResumeButton = () => {
   return (
-    <button className='bg-[#ca8a04] z-10 font-bold py-2 px-9 mr-2 mb-1 rounded-md hover:bg-[#ca6a04]'>            <a
-    className="flex justify-between items-center w-full text-gray-300"
-    href='/Resume.pdf'
-    target='_blank'
-  >
-    Resume
-  </a></button>
+    <a
+      className="flex justify-between items-center w-full text-gray-300"
+      href="/Resume.pdf"
+      target="_blank"
+    >
+      {" "}
+      <button className="bg-[#ca8a04] z-10 font-bold py-2 px-9 mr-2 mb-1 rounded-md hover:bg-[#ca6a04]">
+        Resume
+      </button>
+    </a>
+  );
+};
 
-  )
-}
-
-export default ResumeButton
+export default ResumeButton;
