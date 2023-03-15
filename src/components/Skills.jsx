7224699ade094129b2ba1741fa7045cc10a24cb1
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-15 13:26:13 +0100

    Rearrange order of skills for improved hierarchy

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,7 +8,10 @@ import Typescript from "../assets/typescript.png";
 
 const Skills = () => {
   return (
-    <div name="skills" className="bg-[#262626] z-prop w-full h-screen text-[#bcbcb3]">
+    <div
+      name="skills"
+      className="bg-[#262626] z-prop w-full h-screen text-[#bcbcb3]"
+    >
       {/*Container*/}
       <div className="max-w-[1000px] md:p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="z-prop">
@@ -21,29 +24,33 @@ const Skills = () => {
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center z-prop py-8">
-          <div className="shadow-md z-prop lg:py-5 sm:py-2 mx-4 my-4 shadow-[#040c16] hover:z-20 hover:scale-110 duration-500">
-            <img className="w-24 mx-auto z-prop" src={Javascript} alt="HTML icon" />
-            <p className='pt-4 z-prop text-xl'>Javascript</p>
-          </div>
           <div className="shadow-md z-prop lg:py-5 sm:py-2 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-24 mx-auto" src={ReactImg} alt="HTML icon" />
-            <p className='pt-4 text-xl'>React</p>
+            <p className="pt-4 text-xl">React</p>
           </div>
           <div className="shadow-md z-prop lg:py-5 sm:py-2 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-24 mx-auto" src={Typescript} alt="HTML icon" />
-            <p className='pt-4 text-xl'>Typescript</p>
+            <p className="pt-4 text-xl">Typescript</p>
           </div>
           <div className="shadow-md z-prop lg:py-5 sm:py-2 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-24 mx-auto" src={Tailwind} alt="HTML icon" />
-            <p className='pt-4 text-xl'>Tailwind</p>
+            <p className="pt-4 text-xl">Tailwind</p>
+          </div>
+          <div className="shadow-md z-prop lg:py-5 sm:py-2 mx-4 my-4 shadow-[#040c16] hover:z-20 hover:scale-110 duration-500">
+            <img
+              className="w-24 mx-auto z-prop"
+              src={Javascript}
+              alt="HTML icon"
+            />
+            <p className="pt-4 z-prop text-xl">Javascript</p>
           </div>
           <div className="shadow-md z-prop lg:py-5 sm:py-2 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-24 mx-auto" src={HTML} alt="HTML icon" />
-            <p className='pt-4 z-prop text-xl'>HTML</p>
+            <p className="pt-4 z-prop text-xl">HTML</p>
           </div>
           <div className="shadow-md z-prop lg:py-5 sm:py-2 mx-4 my-4 shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-24 mx-auto" src={CSS} alt="HTML icon" />
-            <p className='pt-4 text-xl'>CSS</p>
+            <p className="pt-4 text-xl">CSS</p>
           </div>
         </div>
       </div>
