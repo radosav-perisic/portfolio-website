69a86593975deddcc6237adf59b3c469ef96b45e
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-20 16:49:58 +0100

    Refactor AltResumeButton with window.open

--- src/components/AltResumeButton.jsx
+++ src/components/AltResumeButton.jsx
@@ -1,17 +1,15 @@
-import React from "react";
-
 const AltResumeButton = () => {
+  const handleButtonClick = () => {
+    window.open("/Resume.pdf", "_blank");
+  };
+
   return (
-    <a
-      className="flex justify-center items-center w-full text-gray-300"
-      href="/Resume.pdf"
-      target="_blank"
+    <button
+      className="bg-[#ca8a04] flex z-prop font-bold py-2.5 px-6  md:py-5 md:px-12 md:mt-12 md:mb-3 md:mr-2  rounded-xl text-xl hover:bg-[#ca6a04]"
+      onClick={handleButtonClick}
     >
-      {" "}
-      <button className="bg-[#ca8a04] flex z-prop font-bold py-5 px-12 mt-12  mr-2 rounded-xl text-xl hover:bg-[#ca6a04]">
-        Resume
-      </button>
-    </a>
+      Resume
+    </button>
   );
 };
 
