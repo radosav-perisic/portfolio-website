f097170e4836b0f2f2bc16b40871e82959c96196
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-06 10:08:35 +0200

    Add hover effect to navbar main menu links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,27 +45,27 @@ const Navbar = () => {
         <ResumeButton href='/Resume.pdf' /> 
         </li>
         <li>
-          <Link to="home" smooth={true} duration={500}>
+          <Link className=" hover:text-[#ca8104]" to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
         <li>
-          <Link to="about" smooth={true} duration={500}>
+          <Link className=" hover:text-[#ca8104]"  to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
         <li>
-          <Link to="skills" smooth={true} duration={500}>
+          <Link className=" hover:text-[#ca8104]"  to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
         <li>
-          <Link to="work" smooth={true} duration={500}>
+          <Link className=" hover:text-[#ca8104]" to="work" smooth={true} duration={500}>
             Work
           </Link>
         </li>
         <li>
-          <Link to="contact" smooth={true} duration={500}>
+          <Link className=" hover:text-[#ca8104]"  to="contact" smooth={true} duration={500}>
             Contact
           </Link>
         </li>
