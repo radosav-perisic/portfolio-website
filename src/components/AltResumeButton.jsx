e4951ccaf3f7c40a0210b496449a5de24fc315b5
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-08 12:51:56 +0200

    Update AltResumeButton hover duration

--- src/components/AltResumeButton.jsx
+++ src/components/AltResumeButton.jsx
@@ -5,7 +5,7 @@ const AltResumeButton = () => {
 
   return (
     <button
-      className="bg-[#ca8a04] flex z-prop font-bold py-2.5 px-6  md:py-5 md:px-12 md:mt-12 md:mb-3 md:mr-2  rounded-xl text-xl hover:bg-[#ca6a04]"
+      className="bg-[#ca8a04] flex z-prop font-bold py-2.5 px-6  md:py-5 md:px-12 md:mt-12 md:mb-3 md:mr-2  rounded-xl text-xl hover:bg-[#bf5606] duration-500"
       onClick={handleButtonClick}
     >
       Resume
