988e0087dfb60165805aaff6c385ed2345be9c16
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-08 12:52:33 +0200

    Update ResumeButton hover duration

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -8,7 +8,7 @@ const ResumeButton = () => {
       target="_blank"
     >
       {" "}
-      <button className="bg-[#ca8a04] z-10 font-bold py-2.5 px-9 mr-2 mb-1 rounded-md hover:bg-[#ca6a04]">
+      <button className="bg-[#ca8a04] z-prop font-bold py-2.5 px-9 mr-2 mb-1 rounded-md hover:bg-[#bf5606] duration-500">
         Resume
       </button>
     </a>
