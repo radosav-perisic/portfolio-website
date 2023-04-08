aec8e87f1935ea6bdb0cf69d4a60a2bc7af82df7
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-08 15:00:34 +0200

    Update Work section styles

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -22,21 +22,21 @@ const Work = () => {
           {/*Grid Item*/}
           <div
             style={{ backgroundImage: `url(${Twitter})`, backgroundSize: 'contain' }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md justify-center items-center mx-auto content-div"
+            className="shadow-xl shadow-[#040c16] hover:scale-105 duration-1000 group container rounded-md justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 z-prop text-center group-hover:opacity-100 duration-1000">
-              <span className="text-2xl z-prop font-bold text-white tracking-wider">
+              <span className="text-4xl z-prop font-bold text-[#1b1b18] tracking-wider">
                 Twitter Clone
               </span>
               <div className="pt-8 z-prop text-center">
                 <a href="https://twitter-clone-alarmovski.vercel.app">
-                  <button className=" text-center z-prop rounded-lg px-4 py-3 my-12 mx-4 bg-white text-gray-700 font-bold text-lg">
+                  <button className=" text-center z-prop rounded-lg px-4 py-3 my-12 mx-4 bg-[#2e2e2b] text-[#bcbcb3] font-bold text-lg">
                     Demo
                   </button>
                 </a>
                 <a href="https://github.com/radosav-perisic/twitter-clone">
-                  <button className="text-center z-prop rounded-lg px-4 py-3 my-12 mx-4 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center z-prop rounded-lg px-4 py-3 my-12 mx-4 bg-[#2e2e2b] text-[#bcbcb3] font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -46,21 +46,21 @@ const Work = () => {
           {/*Grid Item*/}
           <div
             style={{ backgroundImage: `url(${Tesla})`, backgroundSize: 'contain' }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md justify-center items-center mx-auto content-div"
+            className="shadow-xl shadow-[#040c16] hover:scale-105 duration-1000 group container rounded-md justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 text-center group-hover:opacity-100 duration-1000">
-              <span className="text-2xl font-bold text-white tracking-wider">
+              <span className="text-4xl font-bold text-[#1b1b18] tracking-wider">
                 Tesla Clone
               </span>
               <div className="pt-8 text-center">
                 <a href="https://rp-tesla-clone.netlify.app">
-                  <button className="text-center rounded-lg px-4 py-3 my-12 mx-4 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-4 py-3 my-12 mx-4 bg-[#2e2e2b] text-[#bcbcb3] font-bold text-lg">
                     Demo
                   </button>
                 </a>
                 <a href="https://github.com/radosav-perisic/tesla-clone">
-                  <button className="text-center rounded-lg px-4 py-3 my-12 mx-4 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-4 py-3 my-12 mx-4 bg-[#2e2e2b] text-[#bcbcb3] font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -70,21 +70,21 @@ const Work = () => {
           {/*Grid Item*/}
           <div
             style={{ backgroundImage: `url(${Traveler})`, backgroundSize: 'contain' }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md justify-center items-center mx-auto content-div"
+            className="shadow-xl shadow-[#040c16] hover:scale-105 duration-1000 group container rounded-md justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 text-center group-hover:opacity-100 duration-1000">
-              <span className="text-2xl font-bold text-white tracking-wider">
+              <span className="text-4xl font-bold text-[#1b1b18] tracking-wider">
                 Traveler's Oasis
               </span>
               <div className="pt-8 text-center">
                 <a href="https://travelers-oasis.netlify.app">
-                  <button className="text-center rounded-lg px-4 py-3 my-12 mx-4 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-4 py-3 my-12 mx-4 bg-[#2e2e2b] text-[#bcbcb3] font-bold text-lg">
                     Demo
                   </button>
                 </a>
                 <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 my-12 mx-4 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-4 py-3 my-12 mx-4 bg-[#2e2e2b] text-[#bcbcb3] font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -94,21 +94,21 @@ const Work = () => {
           {/*Grid Item*/}
           <div
             style={{ backgroundImage: `url(${TechCart})`, backgroundSize: 'contain' }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md justify-center items-center mx-auto content-div"
+            className="shadow-xl shadow-[#040c16] hover:scale-105 duration-1000 group container rounded-md justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 text-center group-hover:opacity-100 duration-1000">
-              <span className="text-2xl justify-items-center font-bold text-white tracking-wider">
+              <span className="text-4xl justify-items-center font-bold text-[#1b1b18] tracking-wider">
                 Tech Cart
               </span>
               <div className="pt-8 text-center">
                 <a href="https://the-gadget-cart.netlify.app">
-                  <button className="text-center rounded-lg px-4 py-3 my-12 mx-4 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-4 py-3 my-12 mx-4 bg-[#2e2e2b] text-[#bcbcb3] font-bold text-lg">
                     Demo
                   </button>
                 </a>
                 <a href="https://github.com/radosav-perisic/the-gadget-cart">
-                  <button className="text-center rounded-lg px-4 py-3 my-12 mx-4 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-4 py-3 my-12 mx-4 bg-[#2e2e2b] text-[#bcbcb3] font-bold text-lg">
                     Code
                   </button>
                 </a>
