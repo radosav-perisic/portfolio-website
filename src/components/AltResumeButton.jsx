9ed6fb6f3beca1f47b2ac29c1542051c0f3e9646
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-17 13:21:30 +0200

    Update AltResumeButton styles

--- src/components/AltResumeButton.jsx
+++ src/components/AltResumeButton.jsx
@@ -5,7 +5,7 @@ const AltResumeButton = () => {
 
   return (
     <button
-      className="border-[4px] text-[#ffb13c] tracking-wide hover:text-white hover:border-[#bf5606] border-white flex z-prop font-semibold md:py-[16px] md:px-[38px] md:mt-12 md:mb-3 md:mr-2 rounded-sm text-[20px] hover:bg-[#bf5606] bg-[#262626] duration-500"
+      className="border-[4px] text-[#ca8a04] tracking-wide hover:text-white hover:border-[#bf5606] border-white flex z-prop font-bold md:py-[16px] md:px-[38px] md:mt-12 md:mb-3 md:mr-2 rounded-sm text-[20px] hover:bg-[#bf5606] bg-[#262626] duration-500"
       onClick={handleButtonClick}
     >
       Resume
