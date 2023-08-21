6717561ff6df0f4607f4785eb07e7ba578a6c2d6
Radosav Perisic <radosavperisic00@gmail.com>
2023-08-21 19:25:39 +0200

    Update About component styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,19 @@
 const About = () => {
   return (
     <div name="about" className="w-full h-screen background-2 text-[#bcbcb3]">
-      <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="sm:text-right pb-8 pl-4 z-prop">
-          <p className="text-4xl font-bold inline border-b-4 text-[#f1eeee] border-[#ca8a04]">
+      <div className="flex flex-col justify-center md:items-center items-start w-full h-full">
+        <div className="text-left pb-8 pl-4 lg:mt-0 mt-20 z-prop">
+          <p className="text-5xl font-bold text-left inline border-b-4 text-[#ffffff] border-[#ca8a04]">
             About
           </p>
         </div>
         <div className="max-w-[1300px] w-full px-4 grid sm:grid-cols-2 gap-8">
-          <div className="sm:text-right md:text-[55px] md:tracking-wide lg:text-5xl text-[34px] leading-8 font-bold z-prop text-[#f1eeee]">
+          <div className="md:text-right text-left md:text-[45px] md:tracking-wide lg:text-5xl text-[34px] leading-8 font-bold z-prop text-[#f1eeee]">
             <p>
             Hey there, I'm Radosav! It's a pleasure to connect with you. Go ahead and have a look around.
             </p>
           </div>    
-          <div className="z-prop text-[#d0d0c5] font-semibold text-lg">
+          <div className="z-prop text-[#d0d0c5] text-left font-semibold text-lg md:mb-0">
             <p>
               As a front-end engineer, I have abundant experience working with
           JavaScript, React.js, TypeScript, and Tailwind. With a deep understanding of both the theoretical and
