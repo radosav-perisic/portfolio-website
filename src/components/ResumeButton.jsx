0ec14d8873805accf59d332ef47343dde60817ff
Radosav Perisic <radosavperisic00@gmail.com>
2023-08-21 10:53:22 +0200

    Update ResumeButton styles

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -8,7 +8,7 @@ const ResumeButton = () => {
       target="_blank"
     >
       {" "}
-      <button className="border-[3px] text-[#ffb13c] hover:text-white hover:border-[#bf5606] border-white z-prop font-semibold py-[8.5px] px-9 mr-2 mb-1 rounded-sm hover:bg-[#bf5606] duration-500">
+      <button className="border-[4px] text-[#ffb13c] hover:text-white hover:border-[#bf5606] border-white z-prop font-semibold py-[8.5px] px-9 mr-2 mb-1 hover:bg-[#bf5606] duration-500">
         Resume
       </button>
     </a>
