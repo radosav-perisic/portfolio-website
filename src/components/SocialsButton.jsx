bfeb66980806106ec22272680a234f47dd22527f
Radosav Perisic <radosavperisic00@gmail.com>
2023-08-21 11:34:48 +0200

    Adjust SocialsButton styling
    Adjust SocialsButton styling to Match Portfolio button styling along with the buttons in Home component for improved user experience

--- src/components/SocialsButton.jsx
+++ src/components/SocialsButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SocialsButton = () => {
   return (
-    <button className='bg-[#ca8a04] z-10 text-white font-bold mr-2 py-1.5 px-2 rounded-xl'>Socials</button>
+    <button className='bg-[#262626] border-4 border-white z-10 text-[#ec8406ea] font-bold mr-4 py-1.5 px-1.5 '>Socials</button>
   )
 }
 
