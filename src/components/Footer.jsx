b5120296a6504d977041e3e5e8f6b6b023d28fbc
Radosav Perisic <radosavperisic00@gmail.com>
2023-08-21 19:27:00 +0200

    Update Footer component styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { BsLinkedin, BsGithub, BsEnvelopeFill, BsTwitter } from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div className="w-full h-28 flex items-center justify-center bg-[#141413] px-6 text-white">
+    <div className="w-full z-prop h-28 flex items-center md:mt-0 relative justify-center bg-[#141413] px-6 text-white">
       
       <div className="flex md:flex-row flex-col space-x-10 items-center">
       
@@ -19,7 +19,7 @@ const Footer = () => {
         </a>
         </div>
         <p className=" text-gray-300/50 text-center relative font-sans"> 
-         Made by <a href="/Resume.pdf" className="hover:underline text-[#ffb13c]">
+         Made by <a href="/Resume.pdf" className="hover:underline text-[#ec8406ea]">
           <span>Radosav Perisic</span>  
         </a> © 2023
       </p>
